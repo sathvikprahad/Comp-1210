f3f89a48e08ac7adbfa3ed170da2de5d7c0cf28f
sathvikprahad <152408656+sathvikprahad@users.noreply.github.com>
2025-08-07 15:28:14 -0500

    Add files via upload

---
+++ project-01/JLetter.java
@@ -0,0 +1,33 @@
+   /** 
+*This program displays a large block letter J.
+*
+*Project 1
+*@author Sathvik Prahadeeswaran - Fundamentals of Computing 1 - 1210
+*@version 08/23/2022
+*/
+public class JLetter {
+/**.
+*Prints large block letter J
+* @param args Command line arguments - not used. 
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA"); 
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+         
+   
+   
+   
+   
+   
+   
+   }
+}
---
+++ project-01/MyLifeGoals.java
@@ -0,0 +1,28 @@
+/** 
+*This program displays my life goals.
+*
+*Project 1
+*@author Sathvik Prahadeeswaran - Fundamentals of Computing 1 - 1210
+*@version 08/23/2022
+*/
+public class MyLifeGoals {
+/**.
+*Prints life goals
+* @param args Command line arguments - not used. 
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("Sathvik Prahadeeswaran");
+      System.out.println();
+      System.out.println("My short-term life goal is to make a 4.0 this" 
+         + " semester and hopefully set up an internship for the summer."
+         + " I also want to join a club and do intramural sports.");
+      System.out.println("My medium-term life goal is to graduate Auburn"
+         + " University with a Computer Science or Software Engineering degree"
+         + " with work experience gained through the Co-Op program.");
+      System.out.println("My long-term life goal is to have generational"
+         + " wealth, a family with multiple children, and a lasting"
+         + " impact on everyone I have met.");
+   }
+}  
+
